360f713b341c05a752dd9a7b26d805632e0311c9
Pavan Bharat Ahire <124116608+pavanahire96@users.noreply.github.com>
2023-07-10 11:24:21 +0530

    Add files via upload

---
+++ linear_search.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[5], i, num, index;
+    cout<<"Enter 5 Numbers: ";
+    for(i=0; i<5; i++)
+        cin>>arr[i];
+    cout<<"\nEnter a Number to Search: ";
+    cin>>num;
+    for(i=0; i<5; i++)
+    {
+        if(arr[i]==num)
+        {
+            index = i;
+            break;
+        }
+        
+        
+    }
+    
+    cout<<"\nFound at Index No."<<index;
+    cout<<endl;
+    return 0;
+}
